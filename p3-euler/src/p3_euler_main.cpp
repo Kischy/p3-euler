49ca419c8714089e9ccd44f8baf573ed3dd26a68
Christian Häge <christian.haege@gmx.net>
2019-03-10 20:53:24 +0100

    Succesful build at

---
+++ p3-euler/src/p3_euler_main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+int main(int argc, char **argv)
+{
+
+	int p3_answer = 0;
+
+
+	cout << "The answer to the third problem of ProjectEuler.net is: " << p3_answer << endl;
+	   
+
+
+
+	return 0;
+}
+
