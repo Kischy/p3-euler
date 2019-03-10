4eef1cec80923a820da4753b05738c929bdb26cf
Christian Häge <christian.haege@gmx.net>
2019-03-10 21:24:35 +0100

    Succesful build at time: 21:24:35,51

--- p3-euler/src/PrimeChecker.h
+++ p3-euler/src/PrimeChecker.h
@@ -6,14 +6,25 @@ class PrimeChecker
 public:
 	bool is_prime_number(unsigned long long number) const
 	{
-		if (number == 2) return true;
+		if (number <= 3) return numbers_until_three(number);
+
 
 		return false;
 	}
 
 
 private:
-	
+	inline bool is_divisable(unsigned long long number, unsigned long long divisor) const
+	{
+		return (number % divisor) == 0;
+	}
+
+	inline bool numbers_until_three(unsigned long long number) const
+	{
+		if (number == 2 || number == 3) return true;
+
+		return false;
+	}
 
 };
 
