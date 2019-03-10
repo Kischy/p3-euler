4baf3fc7b2a26615dee56fa2e2e66a58331151c9
Christian Häge <christian.haege@gmx.net>
2019-03-11 00:21:08 +0100

    inbetween commit

--- p3-euler-test/Factorize_test.cpp
+++ p3-euler-test/Factorize_test.cpp
@@ -3,11 +3,36 @@
 #include "../p3-euler/src/Factorize.h"
 
 
+class FactorizeChecker : public testing::Test
+{
+public:
+	Factorize factorize;
+};
+
 
 TEST(FactorizeTester, FactorizeClassExists) {
 	Factorize factorize;
 
 	//ASSERT_FALSE(primechecker.is_prime_number(0));
 	//ASSERT_FALSE(primechecker.is_prime_number(1));
+}
+
+TEST_F(FactorizeChecker, FactorizationOfTwo)
+{
+	std::vector<unsigned long long> factors = { 2 };
+	ASSERT_EQ(factorize.factorize(2), factors);
+}
+
+TEST_F(FactorizeChecker, FactorizationOfSix)
+{
+	std::vector<unsigned long long> factors = { 2, 3 };
+	ASSERT_EQ(factorize.factorize(6), factors);
+}
+
+
+TEST_F(FactorizeChecker, FactorizationOf2313214)
+{
+	std::vector<unsigned long long> factors = { 2 , 29 , 39883 };
+	ASSERT_EQ(factorize.factorize(2313214), factors);
 }
 
--- p3-euler/src/Factorize.h
+++ p3-euler/src/Factorize.h
@@ -1,12 +1,51 @@
 #pragma once
 
+#include <vector>
+#include "PrimeChecker.h"
 
 class Factorize
 {
 public:
 
+	std::vector<unsigned long long> factorize(unsigned long long number) 
+	{
+		intern_factors.clear();
+
+		factor_to_intern_vector(number);
+
+		return intern_factors;
+	}
+
+
 
 private:
+	PrimeChecker primechecker;
+
+	std::vector<unsigned long long> intern_factors;
+
+	void factor_to_intern_vector(unsigned long long number)
+	{
+		for (unsigned long long i = 2; i <= number; ++i)
+		{
+			if (primechecker.is_prime_number(i) == true)
+			{
+				if (is_divisable(number, i) == true)
+				{
+					intern_factors.push_back(i);
+					number /= i;
+				}
+			}
+		}
+		
+	}
+
+
+	bool is_divisable(unsigned long long number, unsigned long long divisor)
+	{
+		return (number % divisor) == 0;
+	}
+
+
 
 };
 
