193abb71ca1d175153c37d91a0d75f9aa47b6e07
Christian Häge <christian.haege@gmx.net>
2019-03-10 21:15:04 +0100

    Succesful build at time: 21:15:04,80

--- p3-euler-test/prime_checker_test.cpp
+++ p3-euler-test/prime_checker_test.cpp
@@ -3,11 +3,13 @@
 #include "../p3-euler/src/PrimeChecker.h"
 
 
-public PrimeCheckerTesting : public testing::Test
+class PrimeCheckerTesting : public testing::Test
 {
+public:
 	PrimeChecker primechecker;
 };
 
-TEST_F(PrimeCheckerTesting, TestIfOneIsAPrimeNumber) {
-	ASSERT_TRUE(primechecker.is_prime_number(0));
+TEST_F(PrimeCheckerTesting, TestIfOneOrZeroIsAPrimeNumber) {
+	ASSERT_FALSE(primechecker.is_prime_number(0));
+	ASSERT_FALSE(primechecker.is_prime_number(1));
 }
