93a7dfa851250f155c8ed0ccffdb54ec34d6d232
Christian Häge <christian.haege@gmx.net>
2019-03-10 21:49:05 +0100

    Succesful build at time: 21:49:05,30

--- p3-euler-test/remember_numbers_test.cpp
+++ p3-euler-test/remember_numbers_test.cpp
@@ -1,4 +1,17 @@
 #include "pch.h"
 
-#include "../p3-euler/src/RememberNumbers.h
+#include "../p3-euler/src/RememberNumbers.h"
+
+class RememberNumbersCheckerInt : public testing::Test
+{
+public:
+	RememberNumbers<int,int> remembersnumbers_intint;
+};
+
+
+TEST_F(RememberNumbersCheckerInt, TestIfOneOrZeroIsAPrimeNumber) {
+	//ASSERT_FALSE(primechecker.is_prime_number(0));
+}
+
+
 
--- p3-euler/src/RememberNumbers.h
+++ p3-euler/src/RememberNumbers.h
@@ -1,6 +1,8 @@
 #pragma once
 
+#include "IRememberNumbers.h"
 
+template <typename T_key, typename T_value>
 class RememberNumbers
 {
 	
