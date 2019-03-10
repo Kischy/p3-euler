617336956d5a50278460fa3b33e4f379fcb017e8
Christian Häge <christian.haege@gmx.net>
2019-03-10 23:48:45 +0100

    added factorize class

---
+++ p3-euler-test/Factorize_test.cpp
@@ -0,0 +1,13 @@
+#include "pch.h"
+
+#include "../p3-euler/src/Factorize.h"
+
+
+
+TEST(FactorizeTester, FactorizeClassExists) {
+	Factorize factorize;
+
+	//ASSERT_FALSE(primechecker.is_prime_number(0));
+	//ASSERT_FALSE(primechecker.is_prime_number(1));
+}
+
---
+++ p3-euler/src/Factorize.h
@@ -0,0 +1,12 @@
+#pragma once
+
+
+class Factorize
+{
+public:
+
+
+private:
+
+};
+
